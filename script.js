56be450d69040a5e3bfb541b66c6c5f1eec66ff3
David Etuk <djetuk@gmail.com>
2023-05-02 23:57:25 +0100

    feat: exporting node test function

--- script.js
+++ script.js
@@ -15,4 +15,6 @@ const googleSearch = (searchInput) => {
   return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-console.log(googleSearch("cat"));
+// console.log(googleSearch("cat"));
+
+module.exports = { googleSearch };
